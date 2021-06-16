f62b5ccae07e3eb2d9dc90ee82fefbddcd922128
Kuzma Rezchenko <18575606+Kuzma975@users.noreply.github.com>
2021-06-16 16:47:36 +0300

    Revert

--- vars/prop.groovy
+++ vars/prop.groovy
@@ -1,6 +1,10 @@
 #!groovy
 
-call() {
+def call() {
+
+}
+
+def choiceOfLibraryBranch() {
     def parameter = string(name: 'pipelineSharedLibrary', defaultValue: 'master', description: 'Optional - Git branch for jenkins-shared-library', trim: true)
     return parameter
 }
